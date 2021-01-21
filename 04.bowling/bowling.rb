a0fe166915523f2290183cc73cc670a158b78f3b
kasai441 <kasai441441@gmail.com>
2021-01-21 22:07:27 +0900

    ロジック整理、テストケースはすべて合格

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,24 +1,45 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-class Throwing < Struct.new(:id, :frame, :score, :bonus_count, :bonus, :strike, :spare)
-  def update
-    self.score = 10 if self.score == "X"
-    self.score = self.score.to_i
+class Roll < Struct.new(:id, :frame, :score, :bonus_count, :bonus, :feature)
+
+  def keep_frame(bf)
+    self.frame = bf.frame
   end
 
-  def previous_frame(previous)
-    self.frame = previous.frame
+  # 前スローがストライクあるいは前フレームが2回投げ終わっているか
+  def update_frame?(bf, bf2)
+    bf.feature == :strike || (id >= 2 && bf.frame == bf2.frame)
   end
 
-  def forward_frame
+  def update_frame
     self.frame += 1
   end
 
-  def spare?(previous)
-    # p [previous, self]
+  # 10点をとっていて、フレーム一投目かどうか、第10フレームではないか
+  def strike?(bf)
+    if score == 10
+      id == 0 || (id >= 1 && frame != bf.frame && frame < 9)
+    end
+  end
+
+  def spare?(bf)
+    # puts [bf, self, " "]
     # raise '1投と2投の合計が10以上です'
-    self.score + previous.score == 10
+    # フレーム2投目かどうか、フレームの合計が10かどうか、第10フレームではないか
+    id > 0 && self.frame == bf.frame && self.score + bf.score == 10 && frame < 9
+  end
+
+  # ボーナスを先のスローに提供する
+  def mark_strike(aft, aft2)
+    self.feature = :strike
+    aft.provide_bonus_count
+    aft2.provide_bonus_count
+  end
+
+  def mark_spare(aft)
+    self.feature = :spare
+    aft.provide_bonus_count
   end
 
   def provide_bonus_count
@@ -34,56 +55,46 @@ end
 def main
   input = "X123456789"
   input = "6390038273X9180X645"
-  input = "6390038273X9180XXXX"
-  input = "0X150000XXX518104"
-  # input = "6390038273X9180XX00"
-  # input = "XXXXXXXXXXXX"
+  # input = "6390038273X9180XXXX"
+  # input = "0X150000XXX518104"
+  input = "6390038273X9180XX00"
+  input = "XXXXXXXXXXXX"
   scoreList = input.split(//)
   # パラメータ例外処理
   p scoreList
   # 空のフレーム作成
-  throwings = []
+  rolls = []
   scoreList.each_with_index do |score, idx|
-    throwings << Throwing.new(idx, 0, score, 0, 0, false, false)
-    # p throwings[idx]
+    score = 10 if score == "X"
+    rolls << Roll.new(idx, 0, score.to_i, 0, 0, :none)
+    # p rolls[idx]
   end
-  throwings.each do |throwing|
-    back_1 = throwings[throwing.id - 1]
-    back_2 = throwings[throwing.id - 2]
-    next_1 = throwings[throwing.id + 1]
-    next_2 = throwings[throwing.id + 2]
+  rolls.each do |roll|
+    before_2 = rolls[roll.id - 2]
+    before_1 = rolls[roll.id - 1]
+    after_1 = rolls[roll.id + 1]
+    after_2 = rolls[roll.id + 2]
 
-    throwing.previous_frame(back_1)
-    # ストライク
-    if "X" == throwing.score
-      if throwing.id > 0 && back_1.score != 0
-        throwing.strike = true
-      end
-      throwing.update
-      if (throwing.id >= 2 && back_1.frame == back_2.frame) || back_1.strike
-        throwing.forward_frame
-      end
-      if throwing.frame < 9
-        next_1.provide_bonus_count
-        next_2.provide_bonus_count
-      end
-    elsif ("0".."9").include?(throwing.score) || "X" == throwing.score
-      throwing.update
-      if (throwing.id >= 2 && back_1.frame == back_2.frame) || back_1.strike
-          throwing.forward_frame
-      end
-      # スペア
-      if (throwing.id > 0 && throwing.spare?(back_1)) && throwing.frame < 9
-        throwing.spare = true
-        next_1.provide_bonus_count
-      end
-    else
-      # raise 
+    # フレームを進める判定を行う
+    roll.keep_frame(before_1)
+    if roll.update_frame?(before_1, before_2)
+      roll.update_frame
     end
-    throwing.apply_bonus#(back_1) if throwing.id > 0
+    
+    # puts [before_1, after_1, " "]
+
+    # ストライクとスペアの判定を行う
+    if roll.strike?(before_1)
+      roll.mark_strike(after_1, after_2)
+    elsif roll.spare?(before_1)
+      roll.mark_spare(after_1)
+    end
+    
+    # 提供されたボーナスを適用する
+    roll.apply_bonus
   end
-  throwings.each { |e| puts e }
-  throwings.map { |e| e.score + e.bonus }.inject(:+)
+  rolls.each { |e| puts e }
+  rolls.map { |e| e.score + e.bonus }.inject(:+)
 end
 
 puts main
