6f647b52e1d7c1fe53a4e9bb63b233d372136f66
kasai441 <20lions@gmail.com>
2021-01-22 17:11:11 +0900

    rubocopの修正反映

--- 04.bowling/lib/bowling.rb
+++ 04.bowling/lib/bowling.rb
@@ -1,97 +1,93 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-class Roll < Struct.new(:id, :frame, :score, :bonus, :feature)
-# class Roll < Struct.new(:id, :frame, :score, :bonus_count, :bonus, :feature)
-
-  def keep_frame(bf)
-    self.frame = bf.frame
+Roll = Struct.new(:id, :frame, :score, :bonus, :feature) do
+  def keep_frame(bf1)
+    self.frame = bf1.frame
   end
 
-  # 前スローがストライクあるいは前フレームが2回投げ終わっているか
-  def update_frame?(bf, bf2)
-    bf.feature == :strike || (id >= 2 && bf.frame == bf2.frame)
+  # フレーム更新判定：前スローがストライクあるいは前フレームが2回投げ終わっているか
+  def update_frame?(bf1, bf2)
+    bf1.feature == :strike || (id >= 2 && bf1.frame == bf2.frame)
   end
 
   def update_frame
     self.frame += 1
   end
 
-  # 10点をとっていて、フレーム一投目かどうか、第10フレームではないか
-  def strike?(bf)
-    if score == 10
-      id == 0 || (id >= 1 && frame != bf.frame && frame < 9)
-    end
+  # ストライク判定：10点をとっていて、フレーム一投目かどうか、第10フレームではないか
+  def strike?(bf1)
+    return unless score == 10
+
+    id.zero? || (id >= 1 && self.frame != bf1.frame && self.frame < 9)
   end
 
-  def spare?(bf)
-    # puts [bf, self, " "]
-    # raise '1投と2投の合計が10以上です'
-    # フレーム2投目かどうか、フレームの合計が10かどうか、第10フレームではないか
-    id > 0 && self.frame == bf.frame && self.score + bf.score == 10 && frame < 9
+  # スペア判定：フレームの合計が10で、フレーム2投目かどうか、第10フレームではないか
+  def spare?(bf1)
+    return unless score + bf1.score == 10
+
+    id > 0 && self.frame == bf1.frame && self.frame < 9
   end
 
-  # ボーナスを先のスローに提供する
-  def mark_strike(aft, aft2)
+  # ストライクボーナスを先のスローから取得する
+  def mark_strike(aft1, aft2)
     self.feature = :strike
-    self.bonus += aft.score
+    self.bonus += aft1.score
     self.bonus += aft2.score
   end
 
-  def mark_spare(aft)
+  # スペアボーナスを先のスローから取得する
+  def mark_spare(aft1)
     self.feature = :spare
-    self.bonus += aft.score
+    self.bonus += aft1.score
   end
-
-  # def provide_bonus_count
-  #   self.bonus_count += 1
-  # end
-
-  # def apply_bonus(aft, aft2)
-  #   self.bonus += aft.score
-  #   self.bonus += aft2.score
-  # end
 end
 
-def main(input)
-  scoreList = input.split(//)
+def list_rolls(input)
   # パラメータ例外処理
-  p scoreList
-  # 空のフレーム作成
+  return '引数エラー：引数がnilです' if input.nil?
+  return '引数エラー：数字と"X"以外の引数です' unless input.match?(/^(\d|X)+$/)
+
   rolls = []
-  scoreList.each_with_index do |score, idx|
-    score = 10 if score == "X"
+  input.split(//).each_with_index do |score, idx|
+    score = 10 if score == 'X'
     rolls << Roll.new(idx, 0, score.to_i, 0, :none)
-    # p rolls[idx]
   end
+  rolls
+end
+
+def total_score(rolls)
+  return rolls if rolls.is_a?(String)
+
   rolls.each do |roll|
-    before_2 = rolls[roll.id - 2]
-    before_1 = rolls[roll.id - 1]
-    after_1 = rolls[roll.id + 1]
-    after_2 = rolls[roll.id + 2]
+    bf2 = rolls[roll.id - 2]
+    bf1 = rolls[roll.id - 1]
+    aft1 = rolls[roll.id + 1]
+    aft2 = rolls[roll.id + 2]
 
     # フレームを進める判定を行う
-    roll.keep_frame(before_1)
-    if roll.update_frame?(before_1, before_2)
-      roll.update_frame
-    end
-    
-    # puts [before_1, after_1, " "]
+    roll.keep_frame(bf1)
+    roll.update_frame if roll.update_frame?(bf1, bf2)
 
     # ストライクとスペアの判定を行う
-    if roll.strike?(before_1)
-      roll.mark_strike(after_1, after_2)
-    elsif roll.spare?(before_1)
-      roll.mark_spare(after_1)
+    if roll.strike?(bf1)
+      roll.mark_strike(aft1, aft2)
+    elsif roll.spare?(bf1)
+      roll.mark_spare(aft1)
     end
-    
-    # 提供されたボーナスを適用する
-    # roll.apply_bonus
   end
-  rolls.each { |e| puts e }
   rolls.map { |e| e.score + e.bonus }.inject(:+)
 end
 
-# input = 'X'
-input = 'X6390038273X9180X645'
-puts main(input)
+if ARGV.size.zero?
+  puts '引数エラー：引数が必要です'
+elsif ARGV.size > 1
+  puts '引数エラー：引数はひとつのみです'
+end
+
+input = ARGV[0]
+
+rolls = list_rolls(input)
+
+puts total_score(rolls)
+
--- 04.bowling/test/bowling_test.rb
+++ 04.bowling/test/bowling_test.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require './lib/bowling'
 
 class BowlingTest < Minitest::Test
-  def test_main
-    assert_equal 139, main('6390038273X9180X645')
-    assert_equal 164, main('6390038273X9180XXXX')
-    assert_equal 107, main('0X150000XXX518104')
-    assert_equal 134, main('6390038273X9180XX00')
-    assert_equal 300, main('XXXXXXXXXXXX')
+  def test_list_rolls
+    assert_equal 139, total_score(list_rolls('6390038273X9180X645'))
+    assert_equal 164, total_score(list_rolls('6390038273X9180XXXX'))
+    assert_equal 107, total_score(list_rolls('0X150000XXX518104'))
+    assert_equal 134, total_score(list_rolls('6390038273X9180XX00'))
+    assert_equal 300, total_score(list_rolls('XXXXXXXXXXXX'))
+  end
+
+  def test_argument_error
+    assert_equal '引数エラー：数字と"X"以外の引数です', total_score(list_rolls('abc'))
   end
 end
+
