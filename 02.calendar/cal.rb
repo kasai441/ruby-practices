d0e00c9a85fe9a3c389e0a8fdcc819618a9a59cd
kasai441 <kasai441441@gmail.com>
2021-01-25 16:08:51 +0900

    タブ→空白、キャメルケース→スネークケース、ループ処理フラグ管理を排除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,10 +3,10 @@
 require "date"
 
 # 日付の文字数を２にする
-def twoLetter(str)
-	str = str.to_s
+def keep_two_letter(str)
+  str = str.to_s
   if str.length == 1
-    str = " " + str
+    str = ' ' + str
   else
     str
   end
@@ -17,24 +17,24 @@ disp_month = thatdate.month
 disp_year = thatdate.year
 disp_highlight = true
 
-options = { m: "-m", y: "-y", h: "-h" }
-single_options = ["-h"]
+options = { m: '-m', y: '-y', h: '-h' }
+single_options = ['-h']
 
 # 引数の例外処理
 begin
   ARGV.each_with_index do |e, idx|
-		unless options.values.include?(e)
-		  # オプション以外の引数が最初に与えられた場合
-    	if idx == 0
-				raise StandardError.new('最初の引数はオプションにしてください　オプション： ' + options.values.to_s)
-			# オプション以外の引数がオプション指定のものでない場合
-			elsif !(options.values - single_options).include?(ARGV[idx - 1])
-				raise StandardError.new('オプション以外の引数を指定しないでください　オプション：' + options.values.to_s) 
-			end		
-		end
-	end
+    unless options.values.include?(e)
+      # オプション以外の引数が最初に与えられた場合
+      if idx == 0
+        raise StandardError.new('最初の引数はオプションにしてください　オプション： ' + options.values.to_s)
+      # オプション以外の引数がオプション指定のものでない場合
+      elsif !(options.values - single_options).include?(ARGV[idx - 1])
+        raise StandardError.new('オプション以外の引数を指定しないでください　オプション：' + options.values.to_s) 
+      end    
+    end
+  end
 rescue => e
-  p "不正な引数です"
+  p '不正な引数です'
   p e
   return
 end
@@ -55,43 +55,49 @@ if idx = ARGV.index(options[:h])
 end
 
 # 初期化
-weeks = []
-weeks << (" " * 5) + twoLetter(disp_month) + "月 " + disp_year.to_s + (" " * 9)
-weeks << "日 月 火 水 木 金 土  "
+write_week = ''
+write_week += (' ' * 5) + keep_two_letter(disp_month) + '月 ' + disp_year.to_s + (' ' * 9) + "\n"
+write_week += '日 月 火 水 木 金 土  ' + "\n"
 
 # その月の1日の曜日
 begin
-	write_date = Date.new(disp_year, disp_month, 1)
+  write_date = Date.new(disp_year, disp_month, 1)
 rescue => e
-	p "不正な日付の指定です"
-	p e
-	return
+  p '不正な日付の指定です'
+  p e
+  return
 end
 start_cw = write_date.cwday
 
-# 7日ずつ文字列の配列を作る
-start_flg = false
-while write_date.month == disp_month do
-  write_week = ""
-  7.times do |n|
-    n = 7 if n == 0
-    if write_date.month != disp_month || (start_cw != n && !start_flg)
-      write_week += " " * 2
-    else
-			write_day = twoLetter(write_date.day)
-			# 今日の場合、着色
-	    if write_date == thatdate && disp_highlight 
-				write_day = "\e[7m" + write_day + "\e[0m"	
-			end
-      write_week += write_day
-      write_date += 1
-      start_flg = true
-    end
-    write_week += " "
-  end
-  weeks << write_week + " "
+def write_blank_day(num)
+  str = ''
+  day_space = 3
+  str += ' ' * num * day_space
 end
 
-# 配列を表示
-weeks.each { |e| puts e }
+cw = start_cw
+# 最初の日までの空白
+write_week += write_blank_day(cw)
 
+# 月が変わるまで日付を加える
+while write_date.month == disp_month do
+  write_day = keep_two_letter(write_date.day)
+  # 今日の場合、着色
+  if write_date == thatdate && disp_highlight 
+    write_day = "\e[7m" + write_day + "\e[0m"
+  end
+  write_week += write_day + ' '
+  write_date += 1
+  
+  # 週の折り返し
+  cw += 1
+  if cw == 7
+    cw = 0
+    write_week += " \n"
+  end
+end
+
+# 最後の日から行が終わるまでの空白
+write_week += write_blank_day(7 - cw) + ' ' 
+
+puts write_week
