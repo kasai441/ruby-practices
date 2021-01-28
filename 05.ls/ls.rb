86efdfb9f23d08103e699d17c779fd8f552e555c
kasai441 <kasai441441@gmail.com>
2021-01-28 17:22:47 +0900

    rubocop反映

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -20,20 +20,20 @@ def run_app
     items << d
   end
   files = apply_order_option(items, a, r)
-  l ? list_detail(path, files) : list_name(files) 
+  l ? list_detail(path, files) : list_name(files)
 end
 
-def apply_order_option(items, a, r)
-  # Debian10/bash では"."及び大文字小文字は無視してソートする
+def apply_order_option(items, a_opt, r_opt)
+  # Debian10/bash では'.'及び大文字小文字は無視してソートする
   files = items.sort { |a, b| a.gsub(/^\./, '').downcase <=> b.gsub(/^\./, '').downcase }
   # -a オプション
-  files.delete_if { |f| f.match?(/^\..*/) } if !a
+  files.delete_if { |f| f.match?(/^\..*/) } unless a_opt
   # -r オプション
-  files.reverse! if r  
+  files.reverse! if r_opt
 
   files
 end
- 
+
 def list_detail(path, files)
   path = to_readable(path)
   data = []
@@ -50,74 +50,68 @@ end
 
 def to_readable(path)
   # デフォルトで現在のフォルダを参照
-  path = './' if !path
-  # ディレクトリの最後に"/"がない場合、補完する
-  path += '/' if !path.match?(/.*\/$/)
+  path ||= './'
+  # ディレクトリの最後に'/'がない場合、補完する
+  path += '/' unless path.match?(%r{.*/$})
   path
 end
 
-def get_stat(path, f)
+def get_stat(path, file)
   # Mac/Ruby3.0.0 でFile.statにシンボリックリンクのパスを入れると例外となる(Debian10/Ruby2.7.1ではならない）その場合File.lstatにパスを入れる
-  # File.lstatで例外となる場合、そのまま実行時例外で異常終了とする
-  begin
-    stat = File.stat(path + f)
-  rescue
-    stat = File.lstat(path + f)
-  end
+  # File.lstatで例外となる場合異常終了とする
+  File.stat(path + file)
+rescue SystemCallError
+  File.lstat(path + file)
 end
 
-def get_data(stat, path, f)
+def get_data(stat, path, file)
   datum = []
   datum << to_char_ftype(stat.ftype) + to_char_mode(stat.mode)
-  #p "0%o" % stat.mode
   datum << stat.nlink
   datum << Etc.getpwuid(stat.uid).name
   datum << get_group_name(stat.uid)
   datum << stat.size
-  datum << stat.mtime.strftime("%b %d %H:%M")
-  datum << (datum[0][0] == 'l' ? "#{f} -> #{File.readlink(path + f)}" : f)
+  datum << stat.mtime.strftime('%b %d %H:%M')
+  datum << (datum[0][0] == 'l' ? "#{file} -> #{File.readlink(path + file)}" : file)
   datum
 end
 
 def to_char_ftype(ftype)
   {
-    "file": "-",
-    "directory": "d",
-    "characterSpecial": "c",
-    "blockSpecial": "b",
-    "fifo": "p",
-    "link": "l",
-    "socket": "s",
-    "unknown": "?"
+    "file": '-',
+    "directory": 'd',
+    "characterSpecial": 'c',
+    "blockSpecial": 'b',
+    "fifo": 'p',
+    "link": 'l',
+    "socket": 's',
+    "unknown": '?'
   }[ftype.to_sym]
 end
 
 def to_char_mode(mode)
-  mode = "0%o" % mode
-  mode = mode[-3, 3]
-  mode = mode.split('').map { |m| which_mode(m) }.join
+  mode = format('0%o', mode)[-3, 3]
+  mode.split('').map { |m| which_mode(m) }.join
 end
 
-def which_mode(m)
+def which_mode(mode)
   {
-    "0": "---",
-    "1": "--x",
-    "2": "-w-",
-    "3": "-wx",
-    "4": "r--",
-    "5": "r-x",
-    "6": "rw-",
-    "7": "rwx"
-  }[m.to_sym]
+    "0": '---',
+    "1": '--x',
+    "2": '-w-',
+    "3": '-wx',
+    "4": 'r--',
+    "5": 'r-x',
+    "6": 'rw-',
+    "7": 'rwx'
+  }[mode.to_sym]
 end
 
 def get_group_name(uid)
   # Linuxで作成したファイルのグループがMacにない場合エラーとなるので、その場合、Macのlsコマンドの動作に準じて'staff'というグループとみなす
-  begin
-    Etc.getgrgid(uid).name
-  rescue
-    'staff'
-  end
+  Etc.getgrgid(uid).name
+rescue ArgumentError
+  'staff'
 end
 
 def to_same_width(data)
@@ -125,9 +119,9 @@ def to_same_width(data)
   cols = convert_cols_rows(data)
   cols = maximize_cols(cols)
   # 改行するほど長いファイル名がある場合に全ての行で改行が起きてしまうのを防ぐため、ファイル名のみ、長さを元のままとして、揃えない
-  cols.last.map! { |c| c.strip }
+  cols.last.map!(&:strip)
   # 入れ替えた行列を再度入れ替えて元に戻す
-  convert_cols_rows(cols)  
+  convert_cols_rows(cols)
 end
 
 def list_name(files)
@@ -143,13 +137,13 @@ def divide_to_cols(files)
   cols = files.map { |e| [e] }
   # ターミナルの幅に収まるまで列数を減らしていく 行数を2から一つずつ増やす
   (2..files.size).each do |rows_num|
-    unless within_display?(convert_cols_rows(cols))
-      sliced = files.dup
-      cols.clear
-      cols_num = (files.size / rows_num.to_f).ceil
-      cols_num.times { cols << sliced.slice!(0, rows_num) }
-      cols = maximize_cols(cols)
-    end
+    break if within_display?(convert_cols_rows(cols))
+
+    sliced = files.dup
+    cols.clear
+    cols_num = (files.size / rows_num.to_f).ceil
+    cols_num.times { cols << sliced.slice!(0, rows_num) }
+    cols = maximize_cols(cols)
   end
   cols
 end
@@ -180,9 +174,10 @@ def maximize_cols(cols)
   cols.each do |col|
     max = col.map { |c| c.to_s.size }.max
     result << col.map do |c|
-      if c.is_a?(String)
+      case c
+      when String
         c + ' ' * (max - c.size)
-      elsif c.is_a?(Integer)
+      when Integer
         ' ' * (max - c.to_s.size) + c.to_s
       end
     end
